3b6e0a724b61c45b87386448e787e2199677c281
Shahanawaz8815 <111904731+Shahanawaz8815@users.noreply.github.com>
2025-06-03 07:11:51 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,6 +27,51 @@ let someNumber = 33
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//****************** Operations ******************/
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2); //additional
+// console.log(2-2);// substraction
+// console.log(2*2);// multipracation
+// console.log(2**3); //2 rest to the power 3 
+// console.log(2/3);//divide
+// console.log(5%2);//modulus (remender)
+
+let str1 = "hello";
+let str2 = " hitest"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + 1); //number+number = number
+// console.log("1" + 2); // string+number = string
+// console.log(1 + "2"); // number+string = string
+// console.log("1" + 2 + 2); //string +number + number = string // 122 output
+// console.log(1 + 2 + "2"); //number + number + string =[(number + number) + string]first doing math then treat as string // 32 output
+
+console.log(+true); //it does not give error treat as number because of additional operator
+console.log(+""); 
+
+// console.log(true+); //it give error
+
+let num1, num2, num3
+num1 = num2 =num3 =2+2;
+
+// console.log(num1,num2,num3);
+
+
+
+let a=1;
+// console.log(a++);
+// console.log(a);
+
+let i=5;
+// console.log(++i);
+// console.log(++i);
 
