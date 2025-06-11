214a771d15ac2aade89adde1ece26871ed861000
Shahanawaz8815 <111904731+Shahanawaz8815@users.noreply.github.com>
2025-06-11 12:45:01 +0000

    Stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,6 +35,42 @@ let myObj= {
 const myFunction = function(){
     console.log("Hello World")
 }
-
+console.log(typeof heros);//Object(Array)
 console.log(typeof myFunction)//fuction
 console.log(typeof id)//symbol
+
+//++++++++++++++++++++++++++++++++(STACK,Heap memory )++++++++++++++++++++++++++++++++++++
+
+//1.Stack (Primitive Data)
+// It give you a copy of data that is stored into a variable 
+
+let myYoutube = "MohammadShahanawaz"
+
+let anotherName = myYoutube
+
+anotherName = "ArbazKhan"
+console.log(myYoutube);
+console.log(anotherName);
+
+//2.Heap (Non-Primitive Data)
+//It dose not give copy of data it gives you a reference of data so,it means if we target the same data 
+//by two variable then we chage data in any variable the data will change for both variale bcz it share a original reference of a data.
+
+let userOne = {
+    email:"shahanawaz@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+userTwo.email = "Arbaz@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
